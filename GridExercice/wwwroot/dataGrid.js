232e344d4eb69743692be4efec3062a17098b825
Aurélien BOUDOUX <aurelien@boudoux.fr>
2023-10-04 14:28:40 +0200

    Add project files.

---
+++ GridExercice/wwwroot/dataGrid.js
@@ -0,0 +1,10 @@
+﻿var Grid = Grid || {};
+
+Grid.saveToLocalStorage = function (colName, visibility) {
+    localStorage.setItem(colName, JSON.stringify(visibility));    
+}
+
+Grid.getFromLocalStorage = function (colName) {
+    const jsonString = localStorage.getItem(colName);
+    return JSON.parse(jsonString);
+}
